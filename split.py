7f018ff8bc7a5efe6e0741976cea52aef4d3a966
qfulsher <qfulsher@gmail.com>
2015-09-11 15:12:58 -0700

    added support for analyzing wikipedia pages and extracting track info

--- split.py
+++ split.py
@@ -50,10 +50,63 @@ def timeToSeconds(time):
   return seconds
 
 
+
+def findTrackSection(lines):
+	while lines:
+		line = lines[0]
+		if 'Track listing[edit]' in line:
+			return True
+		else:
+			del lines[0]
+	return False
+
+
+def findTrackTable(lines):
+	while lines:
+		line = lines[0]
+		if 'No.' in line:
+			del lines[0]
+			return True
+		else:
+			del lines[0]
+	return False
+
+
+def writeWikiToTracks(track_times, track_titles):
+	track_file = open('tracks.txt', 'w')
+	text = ""
+	for num in range(len(track_times)):
+		text += (track_times[num] + ' - ' + track_titles[num] + '\n')
+	track_file.seek(0)
+	track_file.write(text)
+	track_file.truncate()
+	track_file.close()
+
+
+def wikiLookup(url):
+	os.system('lynx -dump -nolist \''+ url + '\' > ./.tmp.txt')
+	wiki_file = open('.tmp.txt')
+	lines = wiki_file.readlines()
+	track_times = []
+	track_titles = []
+	if findTrackSection(lines) and findTrackTable(lines):
+		while lines:
+			line = lines[0]
+			read_title = re.search('\"[^\"]*\"', line)
+			read_time = re.search('\d*:\d\d', line)
+			if not read_time:
+				break
+			track_times.append(read_time.group(0))
+			track_titles.append(read_title.group(0))
+			del lines[0]
+	writeWikiToTracks(track_times, track_titles)
+	os.remove('./.tmp.txt')
+
+
 if __name__ == "__main__":
   print("Starting")
 
-  
+
   #arg parsing
   parser = argparse.ArgumentParser(description='Split a single-file mp3 Album into its tracks.')
   group = parser.add_mutually_exclusive_group(required=True)
@@ -63,17 +116,23 @@ if __name__ == "__main__":
   parser.add_argument("-A",  "--album", help="Specify the album that the mp3s will be ID3-tagged with. Default: no tag", default=None)
   parser.add_argument("-t", "--tracks", help="Specify the tracks file. Default: tracks.txt", default="tracks.txt")
   parser.add_argument("-f", "--folder", help="Specify the folder the mp3s will be put in. Default: splits/", default="splits")
+  parser.add_argument("-u", "--wikiurl", help="URL of wikipedia page with album track list table.", default=None)
+
   args = parser.parse_args()
   TRACKS_FILE =  args.tracks
   FILENAME = args.mp3
   YT_URL = args.yt
   ALBUM = args.album
   ARTIST = args.artist
+  WIKI_URL = args.wikiurl
 
   if ALBUM and ARTIST and args.folder == "splits":
     FOLDER = ARTIST + " - " + ALBUM
   else:
     FOLDER = args.folder
+
+  if WIKI_URL:
+    wikiLookup(WIKI_URL)
 
   #create destination folder
   if not os.path.exists(FOLDER):
