f50c56b3eb3357608b7cfea6011f7020403208ce
Taylor Shin <kshin@aps.anl.gov>
2020-08-23 14:26:25 -0400

    it was intended to be 'build' not buil..

--- make_install.bat
+++ make_install.bat
@@ -1,9 +1,9 @@
 @echo off
 rem pip install -U pyinstaller
-pyinstaller .\split.spec
+set PYTHONOPTIMIZE=1 && pyinstaller .\split.spec
 copy /Y .\dist\split.exe .\
 
-if %1 == "buil" GOTO DONE
+if %1 == "build" GOTO DONE
 copy /Y .\split.exe "C:\FFMpeg\bin"
 
 :DONE
