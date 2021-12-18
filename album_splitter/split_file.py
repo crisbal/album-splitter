94b0e62f0235a4e1070eb148ca6f4be1b97b4fbf
Leonardo Cavaletti <impeto.blu@gmail.com>
2021-12-18 19:15:26 +0000

    Fixed cover art being dropped on file split

--- album_splitter/split_file.py
+++ album_splitter/split_file.py
@@ -16,18 +16,15 @@ def split_file(input_file: Path, tracks: List[Track], destination: Path, output_
     for i, track in enumerate(tracks):
         start_timestamp = track.start_timestamp
         end_timestamp = file_duration if i == len(tracks) - 1 else tracks[i + 1].start_timestamp
-        outputs[
-            destination / (f"{track.title}.{output_format}")
-        ] = f"-vn -c copy -ss {start_timestamp} -to {end_timestamp}"
-    split_command = ffmpy.FFmpeg(
-        inputs={
-            str(input_file): "-y -hide_banner -loglevel error -stats"}, 
-            outputs={ str(path): v for path, v in outputs.items()
-        }
-    )
-    try:
-        split_command.run()
-    except:
-        raise Exception("Something went wrong with the splitting procedure. See the error above.") from None
+        output_file = destination / (f"{track.title}.{output_format}")
+            
+        split_command = ffmpy.FFmpeg(
+            inputs={str(input_file): f"-y -hide_banner -loglevel error -stats -ss {start_timestamp} -t {end_timestamp}"},
+            outputs={ str(output_file): "-c copy" }
+        )
+        try:
+            split_command.run()
+        except:
+            raise Exception("Something went wrong with the splitting procedure. See the error above.") from None
 
     return list(outputs.keys())
